f727e2f33c39013b2a10c53414fa79369de7b1fd
Rishabh <rishabhmishra872660@gmail.com>
2025-04-19 17:53:31 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Counter from "./component/counter.jsx";
+
+function App() {
+
+  return (
+    <>
+      <div className='rendercounter'>
+          <Counter />
+      </div>
+    </>
+  )
+}
+
+export default App
